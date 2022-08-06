f357845c5dc305645a601b417baef2c15d4a89b8
nadezdatsygankova <nadiapritchina@gamil.com>
2022-08-06 15:53:11 -0400

    feat: add vatldationResult

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import express from 'express';
 //use for cryptography
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
-import {registerValidation} from './validations/auth.js'
+import {registerValidation} from './validations/auth.js';
+import { validationResult } from 'express-validator';
 
 mongoose
   .connect(
@@ -11,35 +12,43 @@ mongoose
   .catch((err) => console.log('db error', err))
 
 
-const app = express();
+const app = express(); //create express app
 
 //can read json file
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-})
+// app.get('/', (req, res) => {
+//   res.send('hello world');
+// })
 
 app.post('/auth/register',registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()){
+    return res.status(400).json(errors.array())
+  }
+
+  res.json({
+    success: true
+  })
 
 });
 
-// app.post('/auth/login', (req, res) => {
-//   console.log(req.body);
+app.post('/auth/login', (req, res) => {
+  console.log(req.body)
 
-//   //cryptography
-//   const token = jwt.sign(
-//     {
-//       email: req.body.email,
-//       fullName: 'Nadia Tsygankova',
-//     }, 'secret123',
-//   );
 
-//   res.json({
-//     success: true,
-//     token,
-//   });
-// })
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: 'Nadia Tsygankova',
+    }, 'secret123',
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
+})
 app.listen('4444', (err) => {
   if (err) {
     return console.log(err);
