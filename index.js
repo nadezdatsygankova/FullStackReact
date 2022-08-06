6a570c0ec8a40003fe80fd4738d7d56308f7b600
nadezdatsygankova <nadiapritchina@gamil.com>
2022-08-06 18:44:21 -0400

    feat: new show user

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-import express from 'express';
+import express, { request } from 'express';
 //use for cryptography
 import jwt from 'jsonwebtoken';
+
 import mongoose from 'mongoose';
 import {registerValidation} from './validations/auth.js';
 import { validationResult } from 'express-validator';
+import userModel from './modules/User.js';
+import bcrypt from 'bcrypt';
+
+import checkAuth from './utils/checkAuth.js';
 
 mongoose
   .connect(
-    'mongodb+srv://NadiaTsy:Volgograd2015@cluster0.genlcka.mongodb.net/?retryWrites=true&w=majority')
+    'mongodb+srv://NadiaTsy:Volgograd2015@cluster0.genlcka.mongodb.net/blog?retryWrites=true&w=majority')
   .then(() => console.log('db connection'))
   .catch((err) => console.log('db error', err))
 
@@ -17,19 +22,86 @@ const app = express(); //create express app
 //can read json file
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res.send('hello world');
-// })
 
-app.post('/auth/register',registerValidation, (req, res) => {
-  const errors = validationResult(req);
+app.post('/auth/login', async (req, res) => {
+  try {
+    const user = await userModel.findOne({
+      email: req.body.email
+    })
+    if(!user){
+      return res.status(404).json({
+        message: 'User not found'
+      });
+    }
+    const isValidPass = await bcrypt.compare(req.body.password,user._doc.passwordHash);
+    if(!isValidPass){
+      return res.status(400).json({
+        message: 'Invalid login or password'
+      })
+    }
+
+    const token = jwt.sign(
+      {
+      _id:user._id,
+    },
+    'secret123',
+    {
+      expiresIn: '30d',
+    },
+    );
+    const {passwordHash, ...userData} = user._doc;
+    res.json({
+      ...userData,
+      token})
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+    message: "failed to login"
+  })
+  }
+})
+
+app.post('/auth/register',registerValidation, async (req, res) => {
+  try {
+    const errors = validationResult(req);
   if (!errors.isEmpty()){
     return res.status(400).json(errors.array())
   }
 
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const doc = new userModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash:hash ,
+  });
+
+  const user = await doc.save();
+
+  const token = jwt.sign(
+    {
+    _id:user._id,
+  },
+  'secret123',
+  {
+    expiresIn: '30d',
+  },
+  );
+  const {passwordHash, ...userData} = user._doc;
   res.json({
-    success: true
-  })
+    ...userData,
+    token})
+
+
+  } catch (error) {
+  console.log(error)
+  res.status(500).json({
+  message: "failed to register"
+})
+  }
 
 });
 
@@ -48,6 +120,13 @@ app.post('/auth/login', (req, res) => {
     success: true,
     token,
   });
+})
+app.get('/auth/me',checkAuth, async (req, res) => {
+  try {
+
+  } catch (error) {
+
+  }
 })
 app.listen('4444', (err) => {
   if (err) {
--- validations/auth.js
+++ validations/auth.js
@@ -1,8 +1,8 @@
 import { body } from 'express-validator'
 
 export const registerValidation =[
-  body('email').isEmail(),
-  body('password').isLength({min: 5}),
-  body('fullName').isLength({min: 3}),
-  body('avatarUrl').optional().isURL(),
+  body('email', 'invalid email format').isEmail(),
+  body('password', 'invalid password format, length must be a main 5 characters').isLength({min: 5}),
+  body('fullName', 'invalid full name format, length must be a main 3 characters').isLength({min: 3}),
+  body('avatarUrl', 'invalid Url format').optional().isURL(),
 ];
