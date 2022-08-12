cdebe921345b703fd00993f54ec9648719411cef
nadezdatsygankova <nadiapritchina@gamil.com>
2022-08-12 16:33:03 -0400

    fix: storage

--- index.js
+++ index.js
@@ -17,11 +17,14 @@ mongoose
 const app = express(); //create express app
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
     cb(null, 'uploads');
   },
   filename: (_, file, cb) => {
-    cb(null, file.originalname)
-  }
+    cb(null, file.originalname);
+  },
 });
 
 const upload = multer({ storage });
